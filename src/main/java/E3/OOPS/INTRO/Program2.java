1408ee93f9759edadc9c41571ff8c52b9c02aff6
AmreshTripathy <amreshtripathy184@gmail.com>
2024-01-31 17:34:47 +0530

    Program 2 added

---
+++ src/main/java/E3/OOPS/INTRO/Program2.java
@@ -0,0 +1,19 @@
+package E3.OOPS.INTRO;
+
+public class Program2 {
+    public static void main(String[] args) {
+        System.out.println("running main() of Program2");
+    }
+}
+
+class Program3 {
+    public void Test() {
+        System.out.println("running main() of Program3");
+    }
+}
+
+class Program4 {
+    public static void main(String[] args) {
+        System.out.println("running main() of Program4");
+    }
+}
