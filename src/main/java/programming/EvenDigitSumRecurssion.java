f4d426329c0e2671bbcba7bcb6391fbb300334be
AmreshTripathy <amreshtripathy184@gmail.com>
2024-03-25 12:31:36 +0530

    EvenDigitSumRecurssion Modification

--- src/main/java/programming/EvenDigitSumRecurssion.java
+++ src/main/java/programming/EvenDigitSumRecurssion.java
@@ -8,7 +8,17 @@ public class EvenDigitSumRecurssion {
         int n = sc.nextInt();
         sc.close();
 
-        System.out.println(sum(n));
+        System.out.println("sum: " + sum(n));
+        System.out.println("count even: " + countEvenDigit(n));
+    }
+
+    private static int countEvenDigit(int n) {
+        if (n == 0)
+            return 0;
+        else if ((n % 10) % 2 == 0)
+            return 1 + sum(n / 10);
+        else
+            return sum(n / 10);
     }
 
     private static int sum(int n) {
